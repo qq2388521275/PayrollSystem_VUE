d085a64197e99eedc15d26358d9aac7593418c5b
Majunbo <2388521275@qq.com>
2021-09-27 19:21:53 +0800

    hello加入增删改查按钮，打包整合至SpringBoot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-    <h1>App.Vue</h1>
-    <button id="helloBtn" @click="helloBtnClick">hello按钮</button>
-    <a>{{ helloMsg }}</a>
+  <div id="main">
+    <div id="helloQuery">
+      <h1>  工资管理系统</h1>
+      <button id="helloQueryBtn" @click="helloQueryBtnClick">查询</button>
+      <a>{{ helloMsg }}</a>
+    </div>
+
+    <div id="helloAdd">
+      <input placeholder="待插入文本" type="text" v-model="addMsg">
+      <button id="helloAddBtn" @click="helloAddBtnClick">添加</button>
+    </div>
+
+    <div id="helloDelete">
+      <input placeholder="待删除文本" type="text" v-model="deleteMsg">
+      <button id="helloDeleteBtn" @click="helloDeleteBtnClick">删除</button>
+    </div>
+
+    <div id="helloUpdate">
+      <input placeholder="修改前" type="text" v-model="updateOldMsg">
+      <input placeholder="修改后" type="text" v-model="updateNewMsg">
+      <button id="helloUpdateBtn" @click="helloUpdateBtnClick">修改</button>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -12,16 +31,42 @@ export default {
   data() {
     return {
       helloMsg: '',
+      addMsg: '',
+      deleteMsg: '',
+      updateOldMsg: '',
+      updateNewMsg: '',
     }
   },
   methods: {
-    helloBtnClick() {
-      fetch("/api/hello", {
+    helloQueryBtnClick() {
+      fetch("/hello", {
         method: "get",
       }).then(res => {
         return res.json()
       })
         .then(res => this.helloMsg = JSON.stringify(res.content))
+    },
+
+    helloAddBtnClick() {
+      if (this.addMsg === '') {
+        alert("无法插入空值")
+      } else {
+        fetch('/hello/add?msg=' + this.addMsg, {
+          method: 'get',
+        }).then(this.helloQueryBtnClick)
+      }
+    },
+
+    helloDeleteBtnClick() {
+      fetch('/hello/delete?msg=' + this.deleteMsg, {
+        method: 'get',
+      }).then(this.helloQueryBtnClick)
+    },
+
+    helloUpdateBtnClick() {
+      fetch('/hello/update?oldMsg=' + this.updateOldMsg + "&newMsg=" + this.updateNewMsg, {
+        method: 'get',
+      }).then(this.helloQueryBtnClick)
     }
   }
 }
@@ -38,7 +83,7 @@ export default {
   margin-top: 60px;
 }
 
-#helloBtn {
+#helloQueryBtn {
   width: 100px;
   height: 60px;
 }

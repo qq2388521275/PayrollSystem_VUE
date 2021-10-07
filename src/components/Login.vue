a08aef534f61d80a8e82393f5a73eeec2119bd1b
Majunbo <2388521275@qq.com>
2021-10-07 20:57:03 +0800

    登录样式1

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="main">
-
-    <div id="title">
-      <h1>登录系统</h1>
+    <div class="form">
+      <form class="register-form">
+        <input type="text" placeholder="name"/>
+        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="email address"/>
+        <button>create</button>
+        <p class="message">Already registered? <a href="#">Sign In</a></p>
+      </form>
+      <form class="login-form">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>登录</button>
+        <p class="message">PayRoll工资管理系统</p>
+      </form>
     </div>
-
-    <div id="userInput">
-      <div id="userName_Input">
-        <input placeholder="用户名" type="text" v-model="userName">
-      </div>
-      <div id="helloDelete_Input">
-        <input placeholder="密码" type="password" v-model="password">
-      </div>
-    </div>
-
-    <div id="login">
-      <button id="loginBtn" @click="loginBtnClick">登录</button>
-    </div>
-
   </div>
 
 </template>
@@ -39,11 +36,10 @@ export default {
         .then(res => this.checkLogin(res));
     },
     checkLogin(res) {
-      if (JSON.stringify(res.status) === '200'){
+      if (JSON.stringify(res.status) === '200') {
         this.$cookies.set('userName', this.userName);
         this.$router.push('/Sheet');
-      }
-      else {
+      } else {
         alert(JSON.stringify(res.content));
       }
     }
@@ -52,21 +48,89 @@ export default {
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
 #main {
-  position: fixed;
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
   text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
-
-#userInput {
-  display: inline-block;
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
 }
-
-#login {
-  display: inline-block;
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
 }
-
-#loginBtn {
-  width: 100px;
-  height: 60px;
+.form button:hover,.form button:active,.form button:focus {
+  background: #43A047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #EF3B3A;
+}
+body {
+  background: #76b852; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #8DC26F);
+  background: -moz-linear-gradient(right, #76b852, #8DC26F);
+  background: -o-linear-gradient(right, #76b852, #8DC26F);
+  background: linear-gradient(to left, #76b852, #8DC26F);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
